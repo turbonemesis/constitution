412e214acf269f387b6f05606e3637f7996d706c
Chase Gibbons <chase@rentdynamics.com>
2021-04-20 16:03:07 -0600

    added typescript

---
+++ .deploy/email-sending-script.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const fs = require('fs'),
+    filename = 'hello-world.pdf',
+    fileType = 'application/pdf',
+    data = fs.readFileSync('attachments/' + filename);
+
+const msg = {
+    to: 'recipient@example.org',
+    from: 'sender@example.org',
+    subject: 'Hello world',
+    text: 'Hello plain world!',
+    html: '<p>Hello HTML world!</p>',
+    attachments: [
+        {
+            content: data.toString('base64'),
+            filename: filename,
+            type: fileType,
+            disposition: 'attachment',
+        },
+    ],
+};
+
+sgMail
+    .send(msg)
+    .then(() => console.log('Mail sent successfully'))
+    .catch(error => console.error(error.toString()));
